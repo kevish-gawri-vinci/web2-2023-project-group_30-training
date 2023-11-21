95cbeedfe5462bf10a3e02dfd664434b57d5c726
kevish-gawri-vinci <kevish.gawri@student.vinci.be>
2023-11-21 22:23:55 +0100

    add: game pause modal, first step

--- frontend/src/Components/Pages/GamePage.js
+++ frontend/src/Components/Pages/GamePage.js
@@ -1,15 +1,41 @@
 import Phaser from 'phaser';
+// eslint-disable-next-line no-unused-vars
+import {Modal} from 'bootstrap'
 import GameScene from '../Game/GameScene';
 
 let game;
 
 const GamePage = () => {
   const phaserGame = `
-<div id="gameDiv" class="d-flex justify-content-center my-3">
+<div id="gamePageDiv">  
+  <div id="gameDiv" class="d-flex justify-content-center my-3">
+    <button type="button"  id="gamePauseButton" class="" data-bs-toggle="modal" data-bs-target="#pauseModal"> || </button>
+    
+    <div class="modal fade" id="pauseModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content" id="pauseMenu">
+          <div class="modal-header text-center">
+            <h2 class="modal-title w-100">Pause</h2>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close" data-bs-dismiss="modal" id="pauseMenuCloseButton">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            ...
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>`;
-
   const main = document.querySelector('main');
   main.innerHTML = phaserGame;
+
+  const mainWidth = window.innerWidth;
+  const pauseButton = document.getElementById('gamePauseButton');
+  const pauseButtonPosition = ((mainWidth - 800) / 2);
+  pauseButton.style.right = `${pauseButtonPosition + 5}px`;
+
 
   const config = {
     type: Phaser.AUTO,
