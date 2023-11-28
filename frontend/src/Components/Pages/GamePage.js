505ea891b5205bbb95e525e94eb792da76b5230e
kevish-gawri-vinci <kevish.gawri@student.vinci.be>
2023-11-28 01:16:25 +0100

    add: pause menu with routing

--- frontend/src/Components/Pages/GamePage.js
+++ frontend/src/Components/Pages/GamePage.js
@@ -2,6 +2,7 @@ import Phaser from 'phaser';
 // eslint-disable-next-line no-unused-vars
 import {Modal} from 'bootstrap'
 import GameScene from '../Game/GameScene';
+// import { clearPage } from '../../utils/render';
 
 let game;
 
@@ -14,20 +15,45 @@ const GamePage = () => {
     <div class="modal fade" id="pauseModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content" id="pauseMenu">
+
           <div class="modal-header text-center">
             <h2 class="modal-title w-100">Pause</h2>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close" data-bs-dismiss="modal" id="pauseMenuCloseButton">
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
+
           <div class="modal-body">
-            ...
+            <div id="menu" class="container mx-auto">
+              <button class="row menuButtons" data-bs-dismiss="modal" id="continueButton">Reprendre</button>
+              <button class="row menuButtons" id="restartButton">Recommencer</button>
+
+              <div id="confToRestartDiv" style="display: none;">
+                <span id="confToRestartTitle"> Etes-vous sûr(e) ?</span>
+                <div class="row justify-content-around">
+                  <button class="col-4 confButtons" data-bs-dismiss="modal" data-uri="/game">Oui</button>
+                  <button class="col-4 confButtons" id="confRestartReturnButton">Retour</button>
+                </div>
+              </div>
+
+              <button class="row menuButtons">Commandes</button>
+              <button class="row menuButtons" id="exitButton">Quitter le jeu</button>
+
+              <div id="confToExitDiv" style="display: none;">
+                <span id="confToExitTitle"> Etes-vous sûr(e) ?</span>
+                <div class="row justify-content-around">
+                  <button class="col-4 confButtons" data-bs-dismiss="modal" data-uri="/">Oui</button>
+                  <button class="col-4 confButtons" id="confExitReturnButton">Retour</button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
   </div>
 </div>`;
+
   const main = document.querySelector('main');
   main.innerHTML = phaserGame;
 
@@ -57,6 +83,66 @@ const GamePage = () => {
   // therefore destroy any started game prior to recreate it
   if (game) game.destroy(true);
   game = new Phaser.Game(config);
+
+
+  // Onclick of the restart button
+  const restartButton = document.getElementById("restartButton");
+  const confToRestartDiv = document.getElementById("confToRestartDiv");
+  
+  restartButton.addEventListener('click', () => {
+    restartButton.style.display = 'none';
+    confToRestartDiv.style.display = 'block';
+  });
+
+  const restartReturnButton = document.getElementById('confRestartReturnButton');
+    
+  restartReturnButton.addEventListener('click', () => {
+    restartButton.style.display = 'inherit';
+    confToRestartDiv.style.display = 'none';
+  });
+
+  // Onclick of the exit button
+  const exitButton = document.getElementById("exitButton");
+  const confToExitDiv = document.getElementById("confToExitDiv");
+
+  exitButton.addEventListener('click', () => {
+    exitButton.style.display = 'none';
+    confToExitDiv.style.display = 'block';
+  });
+
+  const exitReturnButton = document.getElementById("confExitReturnButton");
+
+  exitReturnButton.addEventListener('click', () => {
+    exitButton.style.display = 'inherit';
+    confToExitDiv.style.display = 'none';
+  });
+
+  // Pause the game upon click of pause button
+
+  pauseButton.addEventListener('click', () => {
+    console.log('test s')
+    game.pause();
+  })
+
+  // Resume, upon click of reprendre, and closing of modal
+
+  const continueButton = document.getElementById("continueButton");
+  const closeMenuButton = document.getElementById("pauseMenuCloseButton");
+
+  continueButton.addEventListener('click', () => {
+    game.resume();
+  });
+
+  closeMenuButton.addEventListener('click', () => {
+    game.resume();
+  });
+
+  // eslint-disable-next-line no-unused-vars
+  const pauseModal = new Modal(document.getElementById('pauseModal'), {
+      keyboard: false,
+      backdrop: false
+  });
+
 };
 
 export default GamePage;
