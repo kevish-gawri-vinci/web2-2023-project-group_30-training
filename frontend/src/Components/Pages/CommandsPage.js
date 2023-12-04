d27f7b356957cad59651c4e8e60a1a2fb4e66deb
kevish-gawri-vinci <kevish.gawri@student.vinci.be>
2023-12-04 18:52:19 +0100

    add: commands page basic setup

--- frontend/src/Components/Pages/CommandsPage.js
+++ frontend/src/Components/Pages/CommandsPage.js
@@ -1,10 +1,24 @@
-import  clearPage from '../../utils/render';
-
 const CommandsPage = () => {
-    clearPage.ClearPage();
-    const main = document.querySelector('main');
-    main.innerHTML = '';
-    main.innerHTML = 'Leaderboard page';
+   const  htmlCommands = `
+<div class="modal fade" id="commandsDiv">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header text-center justify-content-center">
+        <h3 class="modal-title">Commandes</h3>
+      </div>
+
+      <div class="modal-body text-center">
+        <p> tetstuzafeyuzagdyizae </p>
+      </div>
+      <div class="modal-footer">
+        <button id="closeCommandsButton" data-bs-dismiss="modal">Fermer</button>
+      </div>
+    </div>    
+    </div>
+  </div>
+</div>  
+  `;
+  return htmlCommands;
   };
 
   export default CommandsPage;
