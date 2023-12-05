87654a4ed8021404e2fdb58250050751f76ea455
Maxime Issa <maximeissa@MacBook-Pro-de-Maxime.local>
2023-12-05 19:16:54 +0100

    Fix pages and birthdate

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -1,29 +1,46 @@
 // import { Navbar as BootstrapNavbar } from 'bootstrap';
 import Navigate from '../Router/Navigate';
 
-/**
- * Render the Navbar which is styled by using Bootstrap
- * Each item in the Navbar is tightly coupled with the Router configuration :
- * - the URI associated to a page shall be given in the attribute "data-uri" of the Navbar
- * - the router will show the Page associated to this URI when the user click on a nav-link
- */
-
 const Navbar = () => {
   const navbarWrapper = document.querySelector('#navbarWrapper');
   const navbar = `
-  <nav class="navbar navbar-expand-lg">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#" data-uri="/">Zero-G Odyssey</a>
-          <span class="navbar-toggler-icon navbarMenuButton"></span> 
+    <nav class="navbar navbar-expand-lg">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#" data-uri="/">Zero-G Odyssey</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a class="nav-link text-white fs-4" href="#" data-uri="/register">S'inscrire</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white fs-4" href="#" data-uri="/login">Se connecter</a>
+            </li>
+          </ul>
         </div>
-      </nav>
+      </div>
+    </nav>
   `;
-  
+
   navbarWrapper.innerHTML = navbar;
+
+  // Ajouter des gestionnaires d'événements pour rediriger l'utilisateur
+  const registerLink = document.querySelector('[data-uri="/register"]');
+  registerLink.addEventListener('click', () => {
+    Navigate('/register');
+  });
+
+  const loginLink = document.querySelector('[data-uri="/login"]');
+  loginLink.addEventListener('click', () => {
+    Navigate('/login');
+  });
+
   const navbarBrand = document.querySelector('.navbar-brand');
   navbarBrand.addEventListener('click', () => {
     Navigate('/');
-  })
+  });
 };
 
 export default Navbar;
--- frontend/src/Components/Pages/LoginPage.js
+++ frontend/src/Components/Pages/LoginPage.js
@@ -1,11 +1,13 @@
+import Navigate from "../Router/Navigate";
+
 const LoginPage = () => {
-    const main = document.querySelector('main');
-    main.innerHTML = `
+  const main = document.querySelector('main');
+  main.innerHTML = `
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-md-6">
                     <h1 class="text-center">Se connecter</h1>
-                    <form id="loginForm" class="needs-validation" novalidate>
+                    <form id="loginForm" class="needs-validation">
                         <div class="mb-3">
                             <label for="username" class="form-label">Nom d'utilisateur:</label>
                             <input type="text" class="form-control" id="username" name="username" required>
@@ -22,38 +24,31 @@ const LoginPage = () => {
             </div>
         </div>
     `;
-  };
-  
-
 
   const loginForm = document.getElementById('loginForm');
-  if (loginForm) {
-      loginForm.addEventListener('submit', async (event) => {
-          event.preventDefault();
+  loginForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
 
-          const username = document.getElementById('username').value;
-          const password = document.getElementById('password').value;
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
 
-          try {
-              const response = await fetch('/api/auths/login', {
-                  method: 'POST',
-                  headers: {
-                      'Content-Type': 'application/json',
-                  },
-                  body: JSON.stringify({ username, password }),
-              });
-
-              if (response.ok) {
-                window.location.href = '/';
-                  
-              } else {
-                  alert('Nom d\'utilisateur ou mot de passe incorrect');
-              }
-          } catch (error) {
-              console.error('Erreur:', error);
-          }
+    try {
+      const response = await fetch('/api/auths/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
       });
-  }
 
-
+      if (response.ok) {
+        Navigate('/')
+    } else {
+        alert("Nom d'utilisateur ou mot de passe incorrect");
+      }
+    } catch (error) {
+      console.error('Erreur:', error);
+    }
+  });
+};
 export default LoginPage;
