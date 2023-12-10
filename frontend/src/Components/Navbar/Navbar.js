eeff76c409bcc16cf8bcd5601302538ec0e831df
Maxime Issa <maximeissa@MacBook-Pro-de-Maxime.local>
2023-12-10 15:15:34 +0100

    login

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -3,6 +3,54 @@ import Navigate from '../Router/Navigate';
 
 const Navbar = () => {
   const navbarWrapper = document.querySelector('#navbarWrapper');
+
+  // Fonction pour générer le bouton de connexion/déconnexion
+  function logButton() {
+    if (isLoggedIn()) {
+      return `<a class="nav-link text-white fs-4" href="#" id="logout1">Se déconnecter</a>`;
+    }
+      return `
+        <a class="nav-link text-white fs-4" href="#" data-uri="/login">Se connecter</a>
+        ${register()}
+      `;
+    }
+  
+
+  // Fonction pour générer le bouton d'inscription
+  function register() {
+    return !isLoggedIn() ? `<a class="nav-link text-white fs-4" href="#" data-uri="/register">S'inscrire</a>` : '';
+  }
+
+  // Fonction pour gérer la déconnexion
+  function logout() {
+    setUserSessionData(null);
+    logoutuser();
+    reloadNavbar();
+    Navigate('/');
+  }
+
+  // Fonction pour recharger dynamiquement la navbar
+  function reloadNavbar() {
+    Navbar();
+    attachEventListeners();
+  }
+
+  // Fonction pour attacher les écouteurs d'événements
+  function attachEventListeners() {
+    document.querySelectorAll('[data-uri]').forEach(link => {
+      link.addEventListener('click', (event) => {
+        const uri = event.target.getAttribute('data-uri');
+        if (uri) Navigate(uri);
+      });
+    });
+
+    const logoutButton = document.querySelector('#logout1');
+    if (logoutButton) {
+      logoutButton.addEventListener('click', logout);
+    }
+  }
+
+  // HTML de la navbar
   const navbar = `
     <nav class="navbar navbar-expand-lg">
       <div class="container-fluid">
@@ -11,82 +59,16 @@ const Navbar = () => {
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ms-auto">
-        
-        ${logButton()}
-      </ul>
+          <ul class="navbar-nav ms-auto">
+            ${logButton()}
+          </ul>
         </div>
       </div>
     </nav>
   `;
 
   navbarWrapper.innerHTML = navbar;
-
-  // Ajouter des gestionnaires d'événements pour rediriger l'utilisateur
-  const registerLink = document.querySelector('[data-uri="/register"]');
-  if (registerLink) {
-    registerLink.addEventListener('click', () => {
-      Navigate('/register');
-    });
-  }
-
-  const loginLink = document.querySelector('[data-uri="/login"]');
-  if (loginLink) {
-    loginLink.addEventListener('click', () => {
-      Navigate('/login');
-    });
-  }
-
-  const navbarBrand = document.querySelector('.navbar-brand');
-  navbarBrand.addEventListener('click', () => {
-    Navigate('/');
-  });
-
-  // Ajouter le gestionnaire d'événements au niveau du document pour le bouton "Se déconnecter"
-  document.addEventListener('click', (event) => {
-    const logoutButton = event.target.closest('[data-uri="/"]');
-    if (logoutButton) {
-      logout();
-    }
-  });
-
-  function logButton() {
-    let logoption = '';
-    if (isLoggedIn()) {
-      logoption = `<a class="nav-link text-white fs-4" href="#" data-uri="/">Se déconnecter</a>`;
-      console.log(isLoggedIn());
-    } else {
-      logoption = `
-        <a class="nav-link text-white fs-4" href="#" data-uri="/login">Se connecter</a>
-        ${register()}
-      `;
-    }
-
-    return logoption;
-  }
-
-  function register() {
-    let registeroption = '';
-    if (!isLoggedIn()) {
-      registeroption = `<a class="nav-link text-white fs-4" href="#" data-uri="/register">S'inscrire</a>`;
-    }
-
-    return registeroption;
-  }
-
-  function logout() {
-    setUserSessionData(null);
-    logoutuser();
-    reloadNavbar();  // Appel à la fonction pour recharger dynamiquement la barre de navigation
-    Navigate('/');
-  }
-  
-  // Ajoutez cette fonction pour recharger dynamiquement la barre de navigation
-  function reloadNavbar() {
-    const navbarWrapperr = document.querySelector('#navbarWrapper');
-    navbarWrapperr.innerHTML = '';
-    Navbar();  // Appel à la fonction Navbar pour regénérer la barre de navigation mise à jour
-  }
+  attachEventListeners();
 };
 
 export default Navbar;
