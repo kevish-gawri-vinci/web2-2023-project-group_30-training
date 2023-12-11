794e1631a1eb85ebd48f79e5dc76151fbcc3dcc1
edwinkalinda <edwin.musakalinda@student.vinci.be>
2023-12-11 13:45:13 +0100

    Shop update

--- frontend/src/Components/Pages/Shop.js
+++ frontend/src/Components/Pages/Shop.js
@@ -3,6 +3,11 @@ import anime from 'animejs';
 import skin1 from '../../assets/Ship1.png';
 import skin2 from '../../assets/Ship2.png';
 import skin3 from '../../assets/Ship3.png';
+import skin4 from '../../assets/Ship4.png';
+import skin5 from '../../assets/Ship5.png';
+import skin6 from '../../assets/Ship6.png';
+import skin7 from '../../assets/Ship7.png';
+import skin8 from '../../assets/Ship8.png';
 import star from '../../assets/star.png';
 
 const ShopPage = () => {
@@ -15,6 +20,11 @@ const ShopPage = () => {
             <button type="button" data-bs-target="#carouselShopItems" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
             <button type="button" data-bs-target="#carouselShopItems" data-bs-slide-to="1" aria-label="Slide 2"></button>
             <button type="button" data-bs-target="#carouselShopItems" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselShopItems" data-bs-slide-to="3" aria-label="Slide 4"></button>
+            <button type="button" data-bs-target="#carouselShopItems" data-bs-slide-to="4" aria-label="Slide 5"></button>
+            <button type="button" data-bs-target="#carouselShopItems" data-bs-slide-to="5" aria-label="Slide 6"></button>
+            <button type="button" data-bs-target="#carouselShopItems" data-bs-slide-to="6" aria-label="Slide 7"></button>
+            <button type="button" data-bs-target="#carouselShopItems" data-bs-slide-to="7" aria-label="Slide 8"></button>
           </div>
           <div class="carousel-inner h-100">
             <div class="carousel-item active h-100" id="1">
@@ -41,16 +51,49 @@ const ShopPage = () => {
                   </div>
               </div>    
             </div>
-          </div>
-          <div class="carousel-item h-100" id="3">
+            <div class="carousel-item h-100" id="4">
               <div class="h-100 shopItemContainer w-100 justify-content-center">
-                  <img src="${skin3}" class="d-block shopItemImg" alt="...">
+                  <img src="${skin4}" class="d-block shopItemImg" alt="...">
                   <div class="shopItemDesc">
                   <h2 class="shopItemTitle">Prix : 1800 <img src="${star}"></h2>
                   </div>
               </div>    
             </div>
+            <div class="carousel-item h-100" id="5">
+              <div class="h-100 shopItemContainer w-100 justify-content-center">
+                  <img src="${skin5}" class="d-block shopItemImg" alt="...">
+                  <div class="shopItemDesc">
+                  <h2 class="shopItemTitle">Prix : 1800 <img src="${star}"></h2>
+                  </div>
+              </div>    
+            </div>
+            <div class="carousel-item h-100" id="6">
+              <div class="h-100 shopItemContainer w-100 justify-content-center">
+                  <img src="${skin6}" class="d-block shopItemImg" alt="...">
+                  <div class="shopItemDesc">
+                  <h2 class="shopItemTitle">Prix : 1800 <img src="${star}"></h2>
+                  </div>
+              </div>    
+            </div>
+            <div class="carousel-item h-100" id="7">
+              <div class="h-100 shopItemContainer w-100 justify-content-center">
+                  <img src="${skin7}" class="d-block shopItemImg" alt="...">
+                  <div class="shopItemDesc">
+                  <h2 class="shopItemTitle">Prix : 1800 <img src="${star}"></h2>
+                  </div>
+              </div>    
+            </div>
+            <div class="carousel-item h-100" id="8">
+              <div class="h-100 shopItemContainer w-100 justify-content-center">
+                  <img src="${skin8}" class="d-block shopItemImg" alt="...">
+                  <div class="shopItemDesc">
+                  <h2 class="shopItemTitle">Prix : 1800 <img src="${star}"></h2>
+                  </div>
+              </div>    
+            </div>
+            
           </div>
+    
           <button class="carousel-control-prev shopInteractionBtn" type="button" data-bs-target="#carouselShopItems" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Previous</span>
