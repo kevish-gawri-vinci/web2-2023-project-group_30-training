561868fa0552556433cba2e07a02879f7daf3759
Maxime Issa <maximeissa@MacBook-Pro-de-Maxime.local>
2023-12-11 14:21:14 +0100

    a

--- api/routes/users.js
+++ api/routes/users.js
@@ -12,28 +12,9 @@ const { parse } = require('../utils/json');
 const router = express.Router();
 
 /* GET users listing. */
-<<<<<<< HEAD
-
-// Nouvelle route pour obtenir les données de profil d'un utilisateur
-router.get('/profil/:username', (req, res) => {
-  const { username } = req.params;
-  const user = readOneUserFromUsername(username);
-
-  if (!user) {
-    return res.status(404).json({ message: 'Utilisateur non trouvé' });
-  }
-
-  // Renvoie les informations nécessaires pour la page de profil
-  res.json({
-    username: user.username,
-    birthdate: user.birthdate,
-    score: user.score,
-  });
-=======
 router.get('/', (req, res) => {
   const users = readAllUsers();
   res.json(users);
->>>>>>> 794e1631a1eb85ebd48f79e5dc76151fbcc3dcc1
 });
 
 // eslint-disable-next-line consistent-return
