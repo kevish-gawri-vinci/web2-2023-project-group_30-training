1905e00a2c7acd67fa88e4d5c1226cbdf6a53ea3
edwinkalinda <edwin.musakalinda@student.vinci.be>
2023-12-11 14:25:14 +0100

    aaa

--- frontend/src/Components/Pages/ProfilPage.js
+++ frontend/src/Components/Pages/ProfilPage.js
@@ -4,9 +4,6 @@ const { username, email, birthdate } = getUserSessionData();
 
 const ProfilePage = () => {
   const main = document.querySelector('main');
-
-  const age = calculateAge(birthdate);
-
   // Creating HTML structure for the profile
   const profileHTML = `
     <div class="container">
@@ -14,7 +11,6 @@ const ProfilePage = () => {
          <div class="profile">
              <h1 class=text-center>Profile</h1>
              <h2 class=text-center>${username}</h2>
-             <h3 class=text-center>Age: ${age}</h3>
              <h3 class=text-center>Email: ${email}</h3>
              <h3 class="text-center">date de naissance: ${birthdate}</3>
          </div>
@@ -25,21 +21,5 @@ const ProfilePage = () => {
   // Setting the HTML content to the main element
   main.innerHTML = profileHTML;
 };
-function calculateAge(birthday) {
-  const birthDate = new Date(birthday);
-  const currentDate = new Date();
-
-  let age = currentDate.getFullYear() - birthDate.getFullYear();
-  const monthDiff = currentDate.getMonth() - birthDate.getMonth();
-
-  // If the current month is before the birth month or
-  // if the current month is the same as the birth month but the current day is before the birth day
-  if (monthDiff < 0 || (monthDiff === 0 && currentDate.getDate() < birthDate.getDate())) {
-    // eslint-disable-next-line no-plusplus
-    age--;
-  }
-
-  return age;
-}
 
 export default ProfilePage;
